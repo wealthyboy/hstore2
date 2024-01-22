fe4906f4ea6abb599002a5a784fe81f11ccb0aaf
Jacob <jacob.atam@gmail.com>
2024-01-22 15:52:25 -0800

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,6 +41,16 @@ class HomeController extends Controller
         //         ]);
         //     }
         // }
+        foreach ($banners as $c) {
+            // Original URL
+            $originalUrl = "https://hautesignatures.com/images/products/m/8lcARWNWMZCSD533ZDc1cT9P0f03Lq6rkD4tRR2g.jpg";
+
+            // Replace the domain
+            $c->image = str_replace("https://hautesignatures.com", "https://hautesignatures.com.ng", $c->image);
+
+            // Output the new URL
+            $c->save();
+        }
 
         $reviews = Review::where('is_verified', 1)->inRandomOrder()->orderBy('created_at', 'DESC')->take(20)->get();
         $posts = Information::orderBy('created_at', 'DESC')->where(['blog' => true, 'is_active' => true])->take(6)->get();
