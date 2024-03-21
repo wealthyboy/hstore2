6fc49049a174e29224309ef389147bed60962d9a
Jacob <jacob.atam@gmail.com>
2024-03-20 21:15:07 -0700

    fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -22,7 +22,7 @@ use App\Services\Newsletter\Exceptions\UserAlreadySubscribedException;
 
 class RegisterController extends Controller
 {
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Register Controller
     |--------------------------------------------------------------------------
@@ -33,213 +33,209 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+	use RegistersUsers;
 
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-	
+	/**
+	 * Where to redirect users after login / registration.
+	 *
+	 * @var string
+	 */
+
 	protected $redirectTo = '/';
-	
+
 	public $newsletter;
 
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct(Request $request,NewsletterContract $newsletter)
-    {   
-	    //check to SEE IF WE ARE IN ADMIN
-        if ( strpos($request->url() ,'admin') !== false ) { 
-		    $this->middleware('admin'); 
-		} else{
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(Request $request)
+	{
+		//check to SEE IF WE ARE IN ADMIN
+		if (strpos($request->url(), 'admin') !== false) {
+			$this->middleware('admin');
+		} else {
 			$this->middleware('guest');
 		}
-		$this->newsletter = $newsletter;
+		//$this->newsletter = $newsletter;
 
-    }
-	
-	
+	}
+
+
 	/**
-     * Handle a registration request for the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function register(Request $request)
-    {   
-        $this->validator($request->all())->validate();
+	 * Handle a registration request for the application.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function register(Request $request)
+	{
+		$this->validator($request->all())->validate();
 		event(new Registered($user = $this->create($request->all())));
-		
 
-	    if ( $request->is ('admin/*') ) { 
-		    return redirect('/admin/users')->with('status','User Created');
+
+		if ($request->is('admin/*')) {
+			return redirect('/admin/users')->with('status', 'User Created');
 		}
 
 		$this->guard()->login($user);
-        // \Mail::to($user->email)->send(new ConfirmationEmail($user));
+		// \Mail::to($user->email)->send(new ConfirmationEmail($user));
 		// return redirect('/verify');
 		return $this->registered($request, $user)
-                        ?: redirect($this->redirectPath());
-    }
-	
+			?: redirect($this->redirectPath());
+	}
+
 	public function showRegistrationForm(Request $request)
-    {   
-		if ( \Auth::check()) { 
-		   return redirect('/');
+	{
+		if (\Auth::check()) {
+			return redirect('/');
 		}
 		$states = State::all();
-        return view('auth.register',compact('states'));
-    }
+		return view('auth.register', compact('states'));
+	}
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {    
-	    $request = new Request();
-			
-		if ( isset($data['admin'] )){
-		    return Validator::make($data, [
-			   'first_name' => 'required|min:1|max:50',
-			   'email'      => 'required|email|max:255|unique:users',
-			   'password'   => 'required|max:90',
-			   'address'    => 'required|min:1|max:200',
-			   'city'       => 'required|min:1|max:200',
-			   'state'      => 'required'
-            ]);
-		 }  
-		  
-		return Validator::make($data, [
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		$request = new Request();
+
+		if (isset($data['admin'])) {
+			return Validator::make($data, [
 				'first_name' => 'required|min:1|max:50',
-				'last_name'    =>'required|min:1|max:50',
-				'email'        => 'required|email|max:75|unique:users',
-				'phone_number' => 'required',
-				'password'     => 'required|confirmed|max:90',
-				'password_confirmation' => 'required|min:3',
-        ]);
-		  
-    }
-	
-	
-	public function verifyEmail($token='') { 
-	  
-	  $id = User::whereToken($token)->firstOrFail();
-	  
-	    if ($id) { 
-	 
+				'email'      => 'required|email|max:255|unique:users',
+				'password'   => 'required|max:90',
+				'address'    => 'required|min:1|max:200',
+				'city'       => 'required|min:1|max:200',
+				'state'      => 'required'
+			]);
+		}
+
+		return Validator::make($data, [
+			'first_name' => 'required|min:1|max:50',
+			'last_name'    => 'required|min:1|max:50',
+			'email'        => 'required|email|max:75|unique:users',
+			'phone_number' => 'required',
+			'password'     => 'required|confirmed|max:90',
+			'password_confirmation' => 'required|min:3',
+		]);
+	}
+
+
+	public function verifyEmail($token = '')
+	{
+
+		$id = User::whereToken($token)->firstOrFail();
+
+		if ($id) {
+
 			User::whereToken($token)->firstOrFail()->hasVerified();
 			// Login and "remember" the given user...
 			\Auth::loginUsingId($id->id, true);
 			//send coupon
 			$code = str_random(6);
-			Voucher::newUserCoupon(5,$code);
+			Voucher::newUserCoupon(5, $code);
 			$user = \Auth::user();
 			//mail the user 
-			\Mail::to($user->email)->send(new NewUserCoupon($user,$code));
+			\Mail::to($user->email)->send(new NewUserCoupon($user, $code));
 			//create coupon
-			$cookie=\Cookie::get('cart');
-			 
-		    if (\Cookie::get('cart')!== null) {
-				 /* CHECK IF we have a record*/ 
-			    $check = Cart::where('remember_token',\Cookie::get('cart'))->get();
-			    /*Redirect if we dont have a record*/
-			if ( $check->count() ) { 
-			     return redirect('/checkout');
-			    }
-		    }
- 
-		return \Redirect::to('/')->with('status',"Hello  {$id->name} You can continue to explore our services");
-		
-	  }
-	   return view('auth.login');
+			$cookie = \Cookie::get('cart');
+
+			if (\Cookie::get('cart') !== null) {
+				/* CHECK IF we have a record*/
+				$check = Cart::where('remember_token', \Cookie::get('cart'))->get();
+				/*Redirect if we dont have a record*/
+				if ($check->count()) {
+					return redirect('/checkout');
+				}
+			}
+
+			return \Redirect::to('/')->with('status', "Hello  {$id->name} You can continue to explore our services");
+		}
+		return view('auth.login');
 	}
 
 
-	public function verify(Request $request) { 
-        if ( $request->session()->has('user_email')) { 
-	      return view('auth.verify');
-	    }	   
-	   return view('errors.404');
+	public function verify(Request $request)
+	{
+		if ($request->session()->has('user_email')) {
+			return view('auth.verify');
+		}
+		return view('errors.404');
 	}
-	
+
 	/**
-     * The user has been registered.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  mixed  $user
-     * @return mixed
-     */
-    protected function registered(Request $request, $user)
-    {
+	 * The user has been registered.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  mixed  $user
+	 * @return mixed
+	 */
+	protected function registered(Request $request, $user)
+	{
 		return redirect()->intended($this->redirectPath());
-    }
-	
-	
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {    
-		
+	}
+
+
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return User
+	 */
+	protected function create(array $data)
+	{
+
 		$user = new User();
-	    $request = new Request();
-		 //The registration is from admin
-	    if ( isset($data['admin'] )){
+		$request = new Request();
+		//The registration is from admin
+		if (isset($data['admin'])) {
 			$user->name = $data['first_name'];
-			$user->last_name=$data['last_name'];
-			$user->email=$data['email'];
-			$user->verified=true;
-			$user->password=bcrypt($data['password']);
+			$user->last_name = $data['last_name'];
+			$user->email = $data['email'];
+			$user->verified = true;
+			$user->password = bcrypt($data['password']);
 			$user->save();
 			$user->users_permission()->create([
-				'permission_id'=>$data['permission_id']
+				'permission_id' => $data['permission_id']
 			]);
 			$user->addresses()->create([
-				'first_name'=>$data['first_name'],
-				'last_name'=>$data['last_name'],
-				'address'=>$data['address'],
-				'city'=>$data['city'],
-				'state_id'=>$data['state']
+				'first_name' => $data['first_name'],
+				'last_name' => $data['last_name'],
+				'address' => $data['address'],
+				'city' => $data['city'],
+				'state_id' => $data['state']
 			]);
-		   
+
 			return $user;
 		}
 
-		try {
-			$this->newsletter->subscribe(
-				config('services.mailchimp.list'),
-				$data['email']
-			);
-		} catch (UserAlreadySubscribedException $e) {
-			//dd($e->getMessage());
-		}
+		// try {
+		// 	$this->newsletter->subscribe(
+		// 		config('services.mailchimp.list'),
+		// 		$data['email']
+		// 	);
+		// } catch (UserAlreadySubscribedException $e) {
+		// 	//dd($e->getMessage());
+		// }
 
-		Newsletter::create([
-			'email'=> $data['email']
-		]);
+		//Newsletter::create([
+		//'email' => $data['email']
+		//]);
 
-		$user->name=$data['first_name'];
-		$user->last_name=$data['last_name'];
-		$user->email=$data['email'];
+		$user->name = $data['first_name'];
+		$user->last_name = $data['last_name'];
+		$user->email = $data['email'];
 		$user->phone_number = $data['phone_number'];
 		$user->type  = 'subscriber';
-		$user->password=bcrypt($data['password']);
+		$user->password = bcrypt($data['password']);
 		$user->save();
 		return $user;
-    }
-	
-	
-	
-	 
+	}
 }
