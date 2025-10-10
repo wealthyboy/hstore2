0f9a17795c57717dec26e5596e83d406f81f58cf
Jacob <jacob.atam@gmail.com>
2025-10-10 18:02:11 +0100

    fix

--- resources/views/underconstruction/index.blade.php
+++ resources/views/underconstruction/index.blade.php
@@ -23,32 +23,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Roboto:wght@900&display=swap" rel="stylesheet"> 
 
 </head>
-<script>
-  (function(w, d, t, h, s, n) {
-    w.FlodeskObject = n;
-    var fn = function() {
-      (w[n].q = w[n].q || []).push(arguments);
-    };
-    w[n] = w[n] || fn;
-    var f = d.getElementsByTagName(t)[0];
-    var v = '?v=' + Math.floor(new Date().getTime() / (120 * 1000)) * 60;
-    var sm = d.createElement(t);
-    sm.async = true;
-    sm.type = 'module';
-    sm.src = h + s + '.mjs' + v;
-    f.parentNode.insertBefore(sm, f);
-    var sn = d.createElement(t);
-    sn.async = true;
-    sn.noModule = true;
-    sn.src = h + s + '.js' + v;
-    f.parentNode.insertBefore(sn, f);
-  })(window, document, 'script', 'https://assets.flodesk.com', '/universal', 'fd');
-</script>
-<script>
-  window.fd('form', {
-    formId: '62e13ba287802ab6bcaf645a'
-  });
-</script>
+
+
 <style>
 
   /* Set height to 100% for body and html to enable the background image to cover the whole page: */
@@ -158,7 +134,7 @@ hr {
     <p></p>
   </div>
   <div class="middle">
-    <h1> theaurabydora is under maintainance</h1>
+    <h1> {{ config('app.name') }} is under maintainance</h1>
     <p>Please check back later</p>
 
     <hr>
