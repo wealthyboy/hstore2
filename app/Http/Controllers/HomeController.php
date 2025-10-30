bc2cf07121faeaff1ba615c8ee3c40c4623b3528
Jacob <jacob.atam@gmail.com>
2025-10-30 12:55:02 +0100

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,58 +23,48 @@ use Illuminate\Support\Facades\Cookie;
 
 class HomeController extends Controller
 {
-    
+
 
     public function index(Request $request)
     {
-        $site_status = Cache::remember('site_status', 60*30, function () {
-            return Live::first();
-        });
+        $site_status =  Live::first();
 
-        $banners = Cache::remember('banners', 60*30, function () {
-            return Banner::banners()->get();
-        });
+        $banners = Banner::banners()->get();
 
-        $sliders = Cache::remember('sliders', 60*30, function () {
-            return Banner::sliders()->get();
-        });
+        $sliders = Banner::sliders()->get();
 
-        $products = Cache::remember('latest_products', 60*30, function () {
-            return ProductVariation::whereNotNull('price')
+        $products = ProductVariation::whereNotNull('price')
                 ->where('price', '>', 0)
                 ->whereNotNull('name')
                 ->where('name', '!=', '')
-                 ->where('featured',  true)
+                ->where('featured',  true)
                 ->orderBy('updated_at', 'DESC')
                 ->take(8)
                 ->get();
-        });
 
 
-        $reviews = Cache::remember('recent_reviews', 60*30, function () {
-            return Review::where('is_verified', 1)
+        $reviews = Review::where('is_verified', 1)
                 ->inRandomOrder()
                 ->orderBy('created_at', 'DESC')
                 ->take(20)
                 ->get();
-        });
 
-        $posts = Cache::remember('recent_posts', 60*30, function () {
-            return Information::orderBy('created_at', 'DESC')
+        $posts = Information::orderBy('created_at', 'DESC')
                 ->where(['blog' => true, 'is_active' => true])
                 ->take(6)
                 ->get();
-        });
+
+
 
         // under construction logic
         if (!$site_status->make_live) {
             return view('index', compact('sliders', 'banners', 'reviews', 'products', 'posts'));
         } else {
-            if (auth()->check() && auth()->user()->isAdmin()) {
+            if (auth()->ch eck() && auth()->user()->isAdmin()) {
                 return view('index', compact('sliders', 'banners', 'reviews', 'products', 'posts'));
             }
+
             return view('underconstruction.index');
         }
     }
-
 }
